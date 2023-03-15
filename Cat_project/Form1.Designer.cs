a737b75c806385787938c76f9f7137c53720c06a
Anastasiia <akuma.arashi@outlook.com>
2023-03-15 19:32:12 +0300

    button1_Click

--- Cat_project/Form1.Designer.cs
+++ Cat_project/Form1.Designer.cs
@@ -75,7 +75,7 @@ namespace Cat_project
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(464, 21);
+            this.pictureBox1.Location = new System.Drawing.Point(583, 102);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(85, 134);
             this.pictureBox1.TabIndex = 0;
@@ -83,7 +83,7 @@ namespace Cat_project
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(568, 21);
+            this.pictureBox2.Location = new System.Drawing.Point(687, 102);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(85, 134);
             this.pictureBox2.TabIndex = 1;
@@ -92,14 +92,14 @@ namespace Cat_project
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 276);
+            this.dataGridView1.Location = new System.Drawing.Point(131, 357);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(446, 287);
             this.dataGridView1.TabIndex = 2;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(678, 21);
+            this.pictureBox3.Location = new System.Drawing.Point(797, 102);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(85, 134);
             this.pictureBox3.TabIndex = 4;
@@ -107,7 +107,7 @@ namespace Cat_project
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(782, 21);
+            this.pictureBox4.Location = new System.Drawing.Point(901, 102);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(85, 134);
             this.pictureBox4.TabIndex = 3;
@@ -115,7 +115,7 @@ namespace Cat_project
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(881, 21);
+            this.pictureBox5.Location = new System.Drawing.Point(1000, 102);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(85, 134);
             this.pictureBox5.TabIndex = 6;
@@ -123,7 +123,7 @@ namespace Cat_project
             // 
             // pictureBox6
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(985, 21);
+            this.pictureBox6.Location = new System.Drawing.Point(1104, 102);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(85, 134);
             this.pictureBox6.TabIndex = 5;
@@ -131,7 +131,7 @@ namespace Cat_project
             // 
             // pictureBox7
             // 
-            this.pictureBox7.Location = new System.Drawing.Point(464, 206);
+            this.pictureBox7.Location = new System.Drawing.Point(583, 287);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(85, 134);
             this.pictureBox7.TabIndex = 8;
@@ -139,7 +139,7 @@ namespace Cat_project
             // 
             // pictureBox8
             // 
-            this.pictureBox8.Location = new System.Drawing.Point(568, 206);
+            this.pictureBox8.Location = new System.Drawing.Point(687, 287);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(85, 134);
             this.pictureBox8.TabIndex = 7;
@@ -147,7 +147,7 @@ namespace Cat_project
             // 
             // pictureBox9
             // 
-            this.pictureBox9.Location = new System.Drawing.Point(881, 206);
+            this.pictureBox9.Location = new System.Drawing.Point(1000, 287);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(85, 134);
             this.pictureBox9.TabIndex = 10;
@@ -155,7 +155,7 @@ namespace Cat_project
             // 
             // pictureBox10
             // 
-            this.pictureBox10.Location = new System.Drawing.Point(985, 206);
+            this.pictureBox10.Location = new System.Drawing.Point(1104, 287);
             this.pictureBox10.Name = "pictureBox10";
             this.pictureBox10.Size = new System.Drawing.Size(85, 134);
             this.pictureBox10.TabIndex = 9;
@@ -163,7 +163,7 @@ namespace Cat_project
             // 
             // pictureBox11
             // 
-            this.pictureBox11.Location = new System.Drawing.Point(464, 390);
+            this.pictureBox11.Location = new System.Drawing.Point(583, 471);
             this.pictureBox11.Name = "pictureBox11";
             this.pictureBox11.Size = new System.Drawing.Size(85, 134);
             this.pictureBox11.TabIndex = 12;
@@ -171,7 +171,7 @@ namespace Cat_project
             // 
             // pictureBox12
             // 
-            this.pictureBox12.Location = new System.Drawing.Point(568, 390);
+            this.pictureBox12.Location = new System.Drawing.Point(687, 471);
             this.pictureBox12.Name = "pictureBox12";
             this.pictureBox12.Size = new System.Drawing.Size(85, 134);
             this.pictureBox12.TabIndex = 11;
@@ -179,7 +179,7 @@ namespace Cat_project
             // 
             // pictureBox13
             // 
-            this.pictureBox13.Location = new System.Drawing.Point(678, 389);
+            this.pictureBox13.Location = new System.Drawing.Point(797, 470);
             this.pictureBox13.Name = "pictureBox13";
             this.pictureBox13.Size = new System.Drawing.Size(85, 134);
             this.pictureBox13.TabIndex = 14;
@@ -187,7 +187,7 @@ namespace Cat_project
             // 
             // pictureBox14
             // 
-            this.pictureBox14.Location = new System.Drawing.Point(782, 390);
+            this.pictureBox14.Location = new System.Drawing.Point(901, 471);
             this.pictureBox14.Name = "pictureBox14";
             this.pictureBox14.Size = new System.Drawing.Size(85, 134);
             this.pictureBox14.TabIndex = 13;
@@ -195,7 +195,7 @@ namespace Cat_project
             // 
             // pictureBox15
             // 
-            this.pictureBox15.Location = new System.Drawing.Point(881, 390);
+            this.pictureBox15.Location = new System.Drawing.Point(1000, 471);
             this.pictureBox15.Name = "pictureBox15";
             this.pictureBox15.Size = new System.Drawing.Size(85, 134);
             this.pictureBox15.TabIndex = 16;
@@ -203,7 +203,7 @@ namespace Cat_project
             // 
             // pictureBox16
             // 
-            this.pictureBox16.Location = new System.Drawing.Point(985, 390);
+            this.pictureBox16.Location = new System.Drawing.Point(1104, 471);
             this.pictureBox16.Name = "pictureBox16";
             this.pictureBox16.Size = new System.Drawing.Size(85, 134);
             this.pictureBox16.TabIndex = 15;
@@ -214,12 +214,13 @@ namespace Cat_project
             this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Location = new System.Drawing.Point(701, 205);
+            this.button1.Location = new System.Drawing.Point(797, 340);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(174, 45);
             this.button1.TabIndex = 17;
             this.button1.Text = "Погадай мне, Тим";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -237,7 +238,7 @@ namespace Cat_project
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(715, 256);
+            this.textBox1.Location = new System.Drawing.Point(69, 66);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(160, 105);
@@ -247,7 +248,7 @@ namespace Cat_project
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(464, 527);
+            this.label2.Location = new System.Drawing.Point(583, 608);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 20;
@@ -256,7 +257,7 @@ namespace Cat_project
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(464, 348);
+            this.label3.Location = new System.Drawing.Point(583, 429);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 21;
@@ -265,7 +266,7 @@ namespace Cat_project
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(464, 158);
+            this.label4.Location = new System.Drawing.Point(583, 239);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(0, 13);
             this.label4.TabIndex = 22;
@@ -277,7 +278,7 @@ namespace Cat_project
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(54)))), ((int)(((byte)(166)))));
             this.BackgroundImage = global::Cat_project.Properties.Resources.фон_скрин1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(1139, 639);
+            this.ClientSize = new System.Drawing.Size(1227, 674);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
--- Cat_project/Form1.cs
+++ Cat_project/Form1.cs
@@ -22,104 +22,38 @@ namespace Cat_project
             label2.Font = new Font("Tobota", 12, FontStyle.Italic);
             label3.Font = new Font("Tobota", 12, FontStyle.Italic);
             label4.Font = new Font("Tobota", 12, FontStyle.Italic);
-            label1.Text = "Времена нынче тяжелые\n хозяйка заставила оцифроваться\n и на корм зарабатывать \n хотите погадаю?  Если у Вас\n сломался компьютер,\n могу подсказать что с ним";
-           
+            label1.Text = "Времена нынче интересные хозяйка заставила оцифроваться \n и на корм зарабатывать хотите погадаю?";
 
-            pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\cart\\b_06.png");
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            pictureBox7.Visible = false;
+            pictureBox8.Visible = false;
+            pictureBox9.Visible = false;
+            pictureBox10.Visible = false;
+            pictureBox11.Visible = false;
+            pictureBox12.Visible = false;
+            pictureBox13.Visible = false;
+            pictureBox14.Visible = false;
+            pictureBox15.Visible = false;
+            pictureBox16.Visible = false;
+           textBox1.Visible = false;//отладка бд
+            dataGridView1.Visible = false;//отладка
+
+            pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\cart\\b_06.png");//тестовый код
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             Timer timer1 = new Timer();
             timer1.Enabled = true;
             timer1.Interval = 5000;
             timer1.Tick += new EventHandler(timer1_Tick);
 
-            var nums = Enumerable.Range(1, 36).ToList();
-            Karta[] karts = new Karta[16];
-            //int[] result = new int[16];
-            Random rand = new Random();
-            for (int i = 0; i < 16; i++)
-            {
-                int pos = rand.Next(0, nums.Count);
-                karts[i] = new Karta
-                {
-                    Id = nums[pos],
-                    Link = BDcom.GetSqlString("SELECT [link] FROM [dbo].[Card] where [id] =" + nums[pos].ToString()),
-                    Text = BDcom.GetSqlString("SELECT [text] FROM [dbo].[Card] where [id] =" + nums[pos].ToString()),
-                    Sign = Convert.ToInt32(BDcom.GetSqlString("SELECT [etc] FROM [dbo].[Card] where [id] =" + nums[pos].ToString()))
-                };
-                (Controls["pictureBox" + (i+1).ToString()] as PictureBox).Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\cart\\" + karts[i].Link);
-                (Controls["pictureBox" + (i+1).ToString()] as PictureBox).SizeMode = PictureBoxSizeMode.StretchImage;
-                // karts[i] = Karta(nums[pos], BDcom.GetSqlString("SELECT [link] FROM [dbo].[Card] where [id] =" + karts[i - 1].id.ToString());
-                //(nums[pos],);
-                nums.RemoveAt(pos);
-            }
-            foreach (var x in karts)
-            {
-                textBox1.Text+= " "+ x.Sign + Environment.NewLine;
-            }
-            for (int i = 0; i < 16; i++)
-            {
-                if (((i+1)%2==0) && (i!=0)) {
-                    if (((karts[i].Sign == 1) && (1 < karts[i - 1].Sign && 12 > karts[i - 1].Sign)) ||
-                        ((karts[i].Sign == 2) && (karts[i - 1].Sign > 2 && karts[i - 1].Sign < 12)) ||
-                        ((karts[i].Sign == 3) && ((karts[i - 1].Sign == 7) || (karts[i - 1].Sign == 8))) ||
-                        ((karts[i].Sign == 4) && ((karts[i - 1].Sign == 7) || (karts[i - 1].Sign == 8))) ||
-                        ((karts[i].Sign == 5) && ((karts[i - 1].Sign == 4) || (karts[i - 1].Sign > 5 && karts[i - 1].Sign < 11))))
-                    {
-                        (karts[i - 1], karts[i]) = (karts[i], karts[i - 1]);
-                    }
-                    if (((karts[i - 1].Sign == 1) && (1 < karts[i].Sign && 12 > karts[i].Sign)) ||
-                       ((karts[i - 1].Sign == 5) && ((karts[i].Sign == 4) || (karts[i].Sign == 10))))
-                    {
-                        karts[i - 1].Text += " на";
-                        karts[i].Text = BDcom.GetSqlString("SELECT [na] FROM [dbo].[Card] where [id] =" + karts[i].Id.ToString());
-                    }
-                    if (((karts[i - 1].Sign == 2) && (karts[i].Sign > 2 && karts[i].Sign < 12)) ||
-                       ((karts[i - 1].Sign == 5) && (karts[i].Sign > 5 && karts[i].Sign < 9)))
-                    {
-                        karts[i - 1].Text += " к";
-                        karts[i].Text = BDcom.GetSqlString("SELECT [k] FROM [dbo].[Card] where [id] =" + karts[i].Id.ToString());
-                    }
-                    if (((karts[i - 1].Sign == 3) || (karts[i - 1].Sign == 4)) && ((karts[i].Sign == 7) || (karts[i].Sign == 8)))
-                    {
-                        karts[i - 1].Text += " c";
-                        karts[i].Text = BDcom.GetSqlString("SELECT [s] FROM [dbo].[Card] where [id] =" + karts[i].Id.ToString());
-                    }
-                   // if ((karts[i - 1].Sign == 5) && (karts[i].Sign > 5 && karts[i].Sign < 9))
-                   // {
-                   //     karts[i - 1].Text += " к ";
-                   // }
-                  //  if ((karts[i - 1].Sign == 5) && ((karts[i].Sign == 4) || (karts[i].Sign == 10)))
-                //    {
-                 //       karts[i - 1].Text += " на ";
-                  //  }
-                }
-            }
-            textBox1.Text += Environment.NewLine;
-            for (int i = 0; i<16; i++)
-            {
-                //    textBox1.Text += " " + x.Text + Environment.NewLine;
-                if (i < 6)
-                {
-                    label4.Text +=  karts[i].Text + " ";
-                    if (i == 3)
-                        label4.Text += Environment.NewLine;
-                }
-                else
-                    if (i < 10)
-                {
-                    label3.Text += karts[i].Text + " ";
-                    if (i == 7)
-                        label3.Text += Environment.NewLine;
-                }
-                else
-
-                {
-                    label2.Text += karts[i].Text + " ";
-                    if (i == 13)
-                        label2.Text += Environment.NewLine;
-                }
-                // label4.Text += "";
-            }
+           
 
             // foreach (Control x in this.Controls)
             //  {
@@ -177,28 +111,141 @@ namespace Cat_project
             else
                 i = 0;
         }
-        string[] mas = new string[] { "Гадаю я так себе,\n потому что у меня лапки" ,
-            "Просил открыть дверь,\n мне открыли, а это не та\n дверь, пошёл обратно...",
-            "Гадаю я так себе, потому что у меня лапки",
-            "Преставляете хотел\n на солнышке погреться, \n а мне открыли\n не тот балкон...",
-            "Вот уже год питаюсь\n одним кормом и водой, \n а Вы так сможете?",
-            "Недавно узнал, что\n красной точкой управляют...",
-            "На краю стола стоит\n кружка с чаем, так бесяче, \n  пойду её подовину...",
-            "Пойду выражу своё мнение\n об этой программе\n  в лоток...",
-            "Корм опять пропал\n из миски...",
-            "Не разрешают драть\n мои обои...",
-            "Что за люди?! Снова\n на ночь заняли мою кровать...",
-            "Жрать или не жрать,\n вот в чём вопрос...",
-            "Вот народ, сами кормят\n и сами жирным называют\n а я не жирный, это у меня кость пушистая",
-            "Хозяйка отошла и накрыла\n свою тарелку \n с едой плёнкой не хороший всё \n же она человечек...",
-            "Все хотят видеть котика,\n но ни кто не \n спрашивает, хочет ли котик \n видеть всех ",
-            "В осенние холодные деньки,\n всё чаще думаю,\n как хорошо быть толстеньким \n ленивым котиком",
-            "Утром люди выходят,\n а вечером заходят...\n Интересно, что они весь день делают \n за дверью",
-            "По скорее бы новый год,\n огоньки  будут мигать\n вокруг атмосфера чуда и волшебства...\n Ёлку уроню ",
-            "Сидел я сегодня на\n подоконнике, как обычно \n и цветок ни с того ни с сего\n упал в обморок"};
+        string[] mas = new string[] { "Гадаю я так себе, потому что у меня лапки  ╮( ˘ ､ ˘ )╭ " ,
+            "Просил открыть дверь, мне открыли, \n а это не та дверь, пошёл обратно...",
+            "Надеюсь Вы не серьёзно относитесь \n  к гаданиям котика из пикселей?!",
+            "Преставляете хотел на солнышке погреться, \n а мне открыли не тот балкон...╮(-_-)╭",
+            "Вот уже год питаюсь одним кормом и водой, \n а Вы так сможете?",
+            "Недавно узнал, что красной точкой управляют...",
+            "На краю стола стоит кружка с чаем, так бесяче, \n пойду её подовину...",
+            "Пойду выражу своё мнение об этой программе в лоток...",
+            "Корм опять пропал из миски...",
+            "Не разрешают драть мои обои... ଲ(ⓛ ω ⓛ)ଲ",
+            "Что за люди?! Снова на ночь заняли мою кровать...",
+            "Жрать или не жрать, вот в чём вопрос...",
+            "Сами кормят и сами жирным называют...\n А я не жирный, это у меня кость пушистая...",
+            "Хозяйка отошла и накрыла свою тарелку с едой, \n не хороший всё же она человечек...",
+            "Все хотят видеть котика,но ни кто не \n спрашивает, хочет ли котик видеть всех?! ",
+            "В осенние холодные деньки, всё чаще думаю,\n как хорошо быть толстеньким  ленивым котиком",
+            "Утром люди выходят, а вечером заходят...\n Интересно, что они весь день делают  за дверью?!",
+            "По скорее бы новый год...Огоньки будут мигать...\n Вокруг атмосфера чуда и волшебства... Ёлку уроню...(☆ω☆) ",
+            "Сидел я сегодня на подоконнике, как обычно, \n и цветок ни с того ни с сего упал в обморок..."};
         Random nomer = new Random();
-        
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = true;
+            pictureBox7.Visible = true;
+            pictureBox8.Visible = true;
+            pictureBox9.Visible = true;
+            pictureBox10.Visible = true;
+            pictureBox11.Visible = true;
+            pictureBox12.Visible = true;
+            pictureBox13.Visible = true;
+            pictureBox14.Visible = true;
+            pictureBox15.Visible = true;
+            pictureBox16.Visible = true;
+            button1.Text = "Погадай ещё раз";
+
+            var nums = Enumerable.Range(1, 36).ToList();
+            Karta[] karts = new Karta[16];
+            //int[] result = new int[16];
+            Random rand = new Random();
+            for (int i = 0; i < 16; i++)
+            {
+                int pos = rand.Next(0, nums.Count);
+                karts[i] = new Karta
+                {
+                    Id = nums[pos],
+                    Link = BDcom.GetSqlString("SELECT [link] FROM [dbo].[Card] where [id] =" + nums[pos].ToString()),
+                    Text = BDcom.GetSqlString("SELECT [text] FROM [dbo].[Card] where [id] =" + nums[pos].ToString()),
+                    Sign = Convert.ToInt32(BDcom.GetSqlString("SELECT [etc] FROM [dbo].[Card] where [id] =" + nums[pos].ToString()))
+                };
+                (Controls["pictureBox" + (i + 1).ToString()] as PictureBox).Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\cart\\" + karts[i].Link);
+                (Controls["pictureBox" + (i + 1).ToString()] as PictureBox).SizeMode = PictureBoxSizeMode.StretchImage;
+                // karts[i] = Karta(nums[pos], BDcom.GetSqlString("SELECT [link] FROM [dbo].[Card] where [id] =" + karts[i - 1].id.ToString());
+                //(nums[pos],);
+                nums.RemoveAt(pos);
+            }
+            foreach (var x in karts)
+            {
+                textBox1.Text += " " + x.Sign + Environment.NewLine;
+            }
+            for (int i = 0; i < 16; i++)
+            {
+                if (((i + 1) % 2 == 0) && (i != 0))
+                {
+                    if (((karts[i].Sign == 1) && (1 < karts[i - 1].Sign && 12 > karts[i - 1].Sign)) ||
+                        ((karts[i].Sign == 2) && (karts[i - 1].Sign > 2 && karts[i - 1].Sign < 12)) ||
+                        ((karts[i].Sign == 3) && ((karts[i - 1].Sign == 7) || (karts[i - 1].Sign == 8))) ||
+                        ((karts[i].Sign == 4) && ((karts[i - 1].Sign == 7) || (karts[i - 1].Sign == 8))) ||
+                        ((karts[i].Sign == 5) && ((karts[i - 1].Sign == 4) || (karts[i - 1].Sign > 5 && karts[i - 1].Sign < 11))))
+                    {
+                        (karts[i - 1], karts[i]) = (karts[i], karts[i - 1]);
+                    }
+                    if (((karts[i - 1].Sign == 1) && (1 < karts[i].Sign && 12 > karts[i].Sign)) ||
+                       ((karts[i - 1].Sign == 5) && ((karts[i].Sign == 4) || (karts[i].Sign == 10))))
+                    {
+                        karts[i - 1].Text += " на";
+                        karts[i].Text = BDcom.GetSqlString("SELECT [na] FROM [dbo].[Card] where [id] =" + karts[i].Id.ToString());
+                    }
+                    if (((karts[i - 1].Sign == 2) && (karts[i].Sign > 2 && karts[i].Sign < 12)) ||
+                       ((karts[i - 1].Sign == 5) && (karts[i].Sign > 5 && karts[i].Sign < 9)))
+                    {
+                        karts[i - 1].Text += " к";
+                        karts[i].Text = BDcom.GetSqlString("SELECT [k] FROM [dbo].[Card] where [id] =" + karts[i].Id.ToString());
+                    }
+                    if (((karts[i - 1].Sign == 3) || (karts[i - 1].Sign == 4)) && ((karts[i].Sign == 7) || (karts[i].Sign == 8)))
+                    {
+                        karts[i - 1].Text += " c";
+                        karts[i].Text = BDcom.GetSqlString("SELECT [s] FROM [dbo].[Card] where [id] =" + karts[i].Id.ToString());
+                    }
+                    // if ((karts[i - 1].Sign == 5) && (karts[i].Sign > 5 && karts[i].Sign < 9))
+                    // {
+                    //     karts[i - 1].Text += " к ";
+                    // }
+                    //  if ((karts[i - 1].Sign == 5) && ((karts[i].Sign == 4) || (karts[i].Sign == 10)))
+                    //    {
+                    //       karts[i - 1].Text += " на ";
+                    //  }
+                }
+            }
+            textBox1.Text += Environment.NewLine;
+            for (int i = 0; i < 16; i++)
+            {
+                //    textBox1.Text += " " + x.Text + Environment.NewLine;
+                if (i < 6)
+                {
+                    label4.Text += karts[i].Text + " ";
+                    if (i == 3)
+                        label4.Text += Environment.NewLine;
+                }
+                else
+                    if (i < 10)
+                {
+                    label3.Text += karts[i].Text + " ";
+                    if (i == 7)
+                        label3.Text += Environment.NewLine;
+                }
+                else
+
+                {
+                    label2.Text += karts[i].Text + " ";
+                    if (i == 13)
+                        label2.Text += Environment.NewLine;
+                }
+                // label4.Text += "";
+            }
+        }
     }
 
 }
